08bed7d92594564edd3d529fa3ed896eb6604a0d
cynthialu42 <clu2334@gmail.com>
2018-03-30 13:23:18 -0500

    adding styling maybe will add bootstrap for practice

--- main.js
+++ main.js
@@ -12,55 +12,43 @@ $(document).ready(function(){
 
     var database = firebase.database();
     
+    database.ref().on("child_added",function(childSnapshot){
+        let dbTrainName = childSnapshot.val().trainName;
+        let dbTrainDest = childSnapshot.val().trainDest;
+        let dbTrainFirst = moment.utc(childSnapshot.val().trainFirst, "HH:mm");
+        let dbTrainFreq = childSnapshot.val().trainFreq;
+
+        let currentTime = moment();
+        
+        let hourDiff = currentTime.diff(dbTrainFirst, 'hours');
+        let minuteDiff = currentTime.diff(dbTrainFirst, 'minutes'); 
+        let hourDuration = Math.floor(minuteDiff/60);
+        let minuteDuration = minuteDiff % 60;
+
+        let mod = minuteDuration % dbTrainFreq;
+        let minutesLeft = dbTrainFreq - mod;
+
+        let duration = moment.duration({'minute':minutesLeft});
+        let nextTrain = currentTime.add(duration).format('LT');
+
+        $(".js-train-data").append('<tr> <td>'+ dbTrainName + '</td> <td>' + dbTrainDest + '</td> <td>' + dbTrainFreq + '</td> <td>' + nextTrain+ '</td> <td>'+ minutesLeft + '</td>');
+
+    });
     $("#submit").on("click", function(){
         event.preventDefault();
 
         let trainName = $("#name").val().trim();
         let trainDest = $("#destination").val().trim();
-        let trainFirst = moment.utc($("#first").val().trim(),"HH:mm");
+        let trainFirst =$("#first").val().trim();
         let trainFreq = $("#freq").val().trim();
-        let currentTime = moment();
-        
-        let hourDiff = currentTime.diff(trainFirst, 'hours');
-        let minuteDiff = currentTime.diff(trainFirst, 'minutes'); 
-        let hourDuration = Math.floor(minuteDiff/60);
-        let minuteDuration = minuteDiff % 60;
-
-        let mod = minuteDuration % trainFreq;
-        let minutesLeft = trainFreq - mod;
-
-        let duration = moment.duration({'minute':minutesLeft});
-        let nextTrain = currentTime.add(duration).format('LT');
-        // console.log(trainName);
-        // console.log(trainDest);
-        // console.log(trainFreq);
-        // console.log(trainFirst);
-        
-        //var start = moment.utc("12:00", "HH:mm");
-        // This is the frequency of the train
-        //let freq = moment.duration({'minute':15});
-        //let freq = 15;
-        // This should be the current time
-        //let now = moment();
-        //toDate.add(duration);
-        //console.log(start.format('LT'));
-        //console.log(now.format('LT'));
-        // var hourDiff = now.diff(start,'hours');
-        // var minuteDiff = now.diff(start,'minutes');
-        // let hourDuration = Math.floor(minuteDiff/60);
-        // let minuteDuration = minuteDiff % 60;
+        //console.log(trainFirst);
+        database.ref().push({
+            trainName: trainName,
+            trainDest: trainDest,
+            trainFirst: trainFirst,
+            trainFreq: trainFreq
+        });
     
-        // console.log(hourDuration);
-        // console.log(minuteDuration);
-
-        // let mod = minuteDuration % freq;
-        // console.log(mod);
-        // let x = freq - mod;
-        // let duration = moment.duration({'minute':x});
-        // let next = now.add(duration);
-        // console.log(next.format('LT'));
-    
-        $(".js-train-data").append('<tr> <td>'+ trainName + '</td> <td>' + trainDest + '</td> <td>' + trainFreq + '</td> <td>' + nextTrain+ '</td> <td>'+ minutesLeft + '</td>');
         $("#name").val('');
         $("#destination").val('');
         $("#first").val('');
